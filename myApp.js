29aceebd3198087d63df9b8617b2496959086761
AlexVCS <alexcurtisslep@gmail.com>
2021-08-04 19:03:06 -0400

    setting up mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
+const db = process.env.MONGO_URI;
 
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
